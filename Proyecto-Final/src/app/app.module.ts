f1828979ac08749229c1555cf3dd315be1ca7b00
Mercedes Godoy Rojas <mercedesgodoy.code@gmail.com>
2023-03-10 15:47:22 -0300

    header, banner, acerca de

--- Proyecto-Final/src/app/app.module.ts
+++ Proyecto-Final/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { LogoAPComponent } from './componentes/logo-ap/logo-ap.component';
+import { RedesComponent } from './componentes/redes/redes.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { FotoPerfilComponent } from './componentes/titulo/foto-perfil/foto-perfil.component';
+import { NombreComponent } from './componentes/titulo/nombre/nombre.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    RedesComponent,
+    BannerComponent,
+    FotoPerfilComponent,
+    NombreComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
